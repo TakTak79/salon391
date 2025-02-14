4f7e78a11eee2a26704dd416bac76e373233e507
DrewPap <146133816+DrewPap@users.noreply.github.com>
2025-02-14 14:06:07 -0600

    Login in system and basic pages
    Created early login system and empty pages for every link.

--- salonsite/salon/views.py
+++ salonsite/salon/views.py
@@ -1,10 +1,115 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.template import loader
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.models import User
+from .models import *
+
+#from salon.forms import SignUpForm
 
 # Create your views here.
 def index(request):
-    #template = loader.get_template("salonsite/index.html")
     return render(request,'index.html')
-    #return HttpResponse("hello world!")
-    #return HttpResponse(template.render(request))
+
+def about(request):
+    return render(request, 'about.html')
+
+def booking(request):
+    return render(request, 'book.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def shop(request):
+    return render(request, 'shop.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def login_page(request):
+    # Check if the HTTP request method is POST (form submission)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # Check if a user with the provided username exists
+        if not User.objects.filter(username=username).exists():
+            # Display an error message if the username does not exist
+            messages.error(request, 'Invalid Username')
+            return redirect('login_page')
+        
+        # Authenticate the user with the provided username and password
+        user = authenticate(username=username, password=password)
+        
+        if user is None:
+            # Display an error message if authentication fails (invalid password)
+            messages.error(request, "Invalid Password")
+            return redirect('login_page')
+        else:
+            # Log in the user and redirect to the home page upon successful login
+            login(request, user)
+            return redirect('index')
+    
+    # Render the login page template (GET request)
+    return render(request, 'login.html')
+
+# Define a view function for the registration page
+def register_page(request):
+    # Check if the HTTP request method is POST (form submission)
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # Check if a user with the provided username already exists
+        user = User.objects.filter(username=username)
+        
+        if user.exists():
+            # Display an information message if the username is taken
+            messages.info(request, "Username already taken!")
+            return redirect('register')
+        
+        # Create a new User object with the provided information
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username
+        )
+        
+        # Set the user's password and save the user object
+        user.set_password(password)
+        user.save()
+        
+        # Display an information message indicating successful account creation
+        messages.info(request, "Account created Successfully!")
+        return redirect('register')
+    
+    # Render the registration page template (GET request)
+    return render(request, 'register.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change_password.html', {
+        'form': form
+    })
