d6dd00827badc7b0f82228a544ef73f3653fc6d8
DrewPap <146133816+DrewPap@users.noreply.github.com>
2025-02-26 17:15:58 -0600

    Small modifications to model and admin python files

--- salonsite/salon/admin.py
+++ salonsite/salon/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from salon.models import Product, Service
+
 # Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None, {"fields": ["name"]}),
+    (None, {"fields": ["duration"]}),
+    (None,{"fields": ["cost"]}),
+]
+    
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None, {"fields": ["name"]}),
+    (None, {"fields": ["description"]}),
+    (None,{"fields": ["cost"]}),
+]
+
+admin.site.register(Service,ServiceAdmin)
+admin.site.register(Product, ProductAdmin)
--- salonsite/salon/models.py
+++ salonsite/salon/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)  # Service name
+    duration = models.PositiveIntegerField()  # Duration in minutes
+    cost = models.DecimalField(max_digits=6, decimal_places=2) # Cost of Service
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)  # Product name
+    description = models.CharField(max_length=100) # Description of product
+    cost = models.DecimalField(max_digits=6, decimal_places=2) # Cost of Product
